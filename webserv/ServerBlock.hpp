08709eb6863bd548c26c36f877c538ca53167097
Kkuhn1994 <kevkin548@gmail.com>
2025-04-09 21:36:42 +0200

    Add files via upload

---
+++ webserv/ServerBlock.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <string>
+#include "LocationRedirect.hpp"
+
+class ServerBlock
+{
+private:
+    const int port;
+    const std::string serverName;
+    const std::string host;
+    std::map<int, std::string> pathOfErrorFiles;
+    std::vector<LocationRedirect> location;
+    std::string defaultFile;
+}
